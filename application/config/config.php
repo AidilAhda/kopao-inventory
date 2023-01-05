c13a4b6c376e647f1f04d907cadf8f54bd9e570a
AidilAhda <76725012+AidilAhda@users.noreply.github.com>
2023-01-05 21:19:51 +0700

    config

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/kopao-inventory1/';
+$config['base_url'] = 'https://kopao.000webhostapp.com/';
 
 /*
 |--------------------------------------------------------------------------
